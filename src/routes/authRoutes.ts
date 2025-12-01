7b9eb855ce03aeaee9db2750d275e656590d6578
Ahmed M. Ezzeldeen <Ahmed.Mohamed.Ezzeldeen@Gmail.com>
2025-12-01 03:45:41 +0200

    feat: add refresh token route with verification middleware

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import { signup, signupValidation, login, loginValidation } from '../controllers/authController';
+import { signup, signupValidation, login, loginValidation, refreshToken, logout } from '../controllers/authController';
 import { asyncWrapper } from '../middleware/asyncWrapper';
+import { verifyRefToken } from '../middleware/verifyToken';
 
 const router = Router();
 
 router.post('/signup', signupValidation, asyncWrapper(signup));
 router.post('/login', loginValidation, asyncWrapper(login));
+router.get('/refresh-token', verifyRefToken, asyncWrapper(refreshToken));
 
 export default router;
