b4649309e834cadf89b8b9923d48b2769e855be2
Ahmed M. Ezzeldeen <Ahmed.Mohamed.Ezzeldeen@Gmail.com>
2025-12-01 03:45:08 +0200

    feat: implement role-based access control middleware

---
+++ src/middleware/allowedTo.ts
@@ -0,0 +1,14 @@
+import { Request, Response, NextFunction } from 'express';
+import AppError from "./errorHandler";
+import { Role, User } from '../generated/prisma';
+
+const allowedTo = (...roles: Role[]) => {
+  return (req: Request & { currentUser: User }, res: Response, next: NextFunction) => {
+    if (!roles.includes(req.currentUser.role)) {
+      return next(AppError.create('This role is not authorized', 401))
+    }
+    next();
+  };
+}
+
+export default allowedTo;
