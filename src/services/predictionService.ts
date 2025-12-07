70ab0bced42929028b99c71baf7926629b66b517
Ahmed M. Ezzeldeen <Ahmed.Mohamed.Ezzeldeen@Gmail.com>
2025-12-07 21:44:02 +0200

    refactor: simplify AiService constructor by removing unused parameters

--- src/services/predictionService.ts
+++ src/services/predictionService.ts
@@ -3,14 +3,11 @@ import { getFastAPI } from "../ai/client";
 class AiService {
   apiClient = getFastAPI();
 
-  constructor(
-    private aiServerUrl: string,
-    private aiServerUsername: string,
-    private aiServerPassword: string,
-  ) {}
+  constructor() {}
 
   async predict(file: Blob) {
     return this.apiClient.predictPredictPost({ input: file });
   }
 }
 
+export const aiService = new AiService();
