a8860d62a55f758737b3fd8d8ad607acc16ae7ae
Ahmed M. Ezzeldeen <Ahmed.Mohamed.Ezzeldeen@Gmail.com>
2025-12-08 22:30:12 +0200

    feat: enhance createScan to include user authentication and save prediction history

--- src/controllers/prediction.controller.ts
+++ src/controllers/prediction.controller.ts
@@ -1,54 +1,107 @@
 import { Request, Response, NextFunction } from "express";
 import { aiService } from "../services/prediction.service";
 import { PredictOutput } from "../ai/client";
+import { uploadToS3 } from "../lib/s3";
+import { prisma } from "../lib/prisma";
+import AppError from '../middleware/errorHandler';
 
 export const createScan = async (
-  req: Request & { file?: Express.Multer.File },
+  req: Request & { file?: Express.Multer.File , currentUser?: { id: string } },
   res: Response,
   next: NextFunction
 ) => {
-  try {
-    if (!req?.file) {
-      return res.status(400).json({
-        success: false,
-        message: "No image file provided. Use field name 'image'.",
-      });
-    }
-    // This is the correct, type-safe way
-    const imageBlob = new Blob([new Uint8Array(req.file.buffer)], {
-      type: req.file.mimetype || "image/jpeg",
-    });
-
-    const aiResponse: PredictOutput = await aiService.predict(imageBlob);
-
-    // TODO: Save scan result to database
-
-    if (!aiResponse || !aiResponse.label) {
-      return res.status(500).json({
-        success: false,
-        message: "AI prediction failed",
-      });
-    }
-
-    return res.status(201).json({
-      success: true,
-      message: "Leaf image uploaded successfully",
-      data: {
-        prediction: aiResponse.label,
-        probabilities: {
-          blight: aiResponse.probability_blight,
-          common_rust: aiResponse.probability_common_rust,
-          gray_leaf_spot: aiResponse.probability_gray_leaf_spot,
-          healthy: aiResponse.probability_healthy,
-        },
-      },
-    });
-  } catch (error: any) {
-    console.error("Upload error:", error);
-    return res.status(500).json({
+  if (!req?.file) {
+    return res.status(400).json({
       success: false,
-      message: "Failed to upload image",
-      error: process.env.NODE_ENV === "development" ? error.message : undefined,
+      message: "No image file provided. Use field name 'image'.",
     });
   }
+  const userId = req.currentUser?.id;
+
+  const imageBlob = new Blob([new Uint8Array(req.file.buffer)], {
+    type: req.file.mimetype || "image/jpeg",
+  });
+
+  const aiResponse: PredictOutput = await aiService.predict(imageBlob);
+
+  const { url, key } = await uploadToS3(req.file, "scans");
+  const uploadedImage = await prisma.uploadedImage.create({
+    data: {
+      userId,
+      url,
+      key,
+      filename: req.file.originalname,
+      mimeType: req.file.mimetype,
+      size: req.file.size,
+    },
+  });
+
+  if (!aiResponse || !aiResponse.label) {
+    const error = AppError.create("AI prediction failed but image uploaded successfully we will predict it later stay tuned", 500);
+    return next(error);
+  }
+
+  const diseaseNameMap: Record<string, string> = {
+    blight: "Blight",
+    common_rust: "Common Rust",
+    gray_leaf_spot: "Gray Leaf Spot",
+    healthy: "Healthy",
+  };
+
+  const disease = await prisma.disease.findUnique({
+    where: { nameEn: diseaseNameMap[aiResponse.label] },
+  });
+
+  if (!disease) {
+    const error = AppError.create("Disease not found in DB", 500);
+    return next(error);
+  }
+
+  // Create prediction record aka Scan History
+  const prediction = await prisma.prediction.create({
+    data: {
+      imageId: uploadedImage.id,
+      userId,
+      diseaseId: disease.id,
+      confidence: Math.max(
+        aiResponse.probability_blight ?? 0,
+        aiResponse.probability_common_rust ?? 0,
+        aiResponse.probability_gray_leaf_spot ?? 0,
+        aiResponse.probability_healthy ?? 0
+      ),
+      severity: disease.severityLevel,
+      predictedAt: new Date(),
+    },
+    include: {
+      disease: {
+        include: { treatments: true },
+      },
+    },
+  });
+
+  return res.status(201).json({
+    success: true,
+    message: "Corn Leaf Scan created successfully",
+    data: {
+      image: {
+        id: uploadedImage.id,
+        url: uploadedImage.url,
+      },
+      prediction: {
+        disease: {
+          nameEn: disease.nameEn,
+          nameAr: disease.nameAr,
+          severity: disease.severityLevel,
+        },
+        confidence: prediction.confidence,
+        details: prediction.disease.treatments,
+      },
+      otherProbabilities: {
+        blight: aiResponse.probability_blight,
+        common_rust: aiResponse.probability_common_rust,
+        gray_leaf_spot: aiResponse.probability_gray_leaf_spot,
+        healthy: aiResponse.probability_healthy,
+      },
+    },
+  });
 };
--- src/routes/prediction.routes.ts
+++ src/routes/prediction.routes.ts
@@ -2,10 +2,11 @@ import { Router } from 'express';
 import { asyncWrapper } from '../middleware/asyncWrapper';
 import { createScan } from '../controllers/prediction.controller';
 import { uploadMemory } from '../middleware/upload';
+import { verifyToken } from '../middleware/verifyToken';
 
 const router = Router();
 
-router.post('/create-scan', uploadMemory.single('image') ,  asyncWrapper(createScan));
+router.post('/create-scan', verifyToken , uploadMemory.single('image') , asyncWrapper(createScan));
 
 
 export default router;
